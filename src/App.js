307ed71c1f37cf425dc7f80b120d1ed731282b57
pranav <pranavverma550@gmail.com>
2019-01-31 14:47:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      regDet: {}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.fnClick = this.fnClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      regDet: {
+        ...this.state.regDet,
+        [e.target.name]:
+          e.target.type == "checkbox" ? e.target.checked : e.target.value
+      }
+    });
+  }
+
+  fnClick() {
+    console.log(this.state.regDet);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Contact Form</h2>
+        <div>
+          Username:
+          <input type="text" name="username" onChange={this.handleChange} />
+        </div>
+        <div>
+          Password:
+          <input type="password" name="password" onChange={this.handleChange} />
+        </div>
+        <div>
+          Gender:
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            onChange={this.handleChange}
+          />
+          Male
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            onChange={this.handleChange}
+          />
+          Female
+        </div>
+
+        <div>
+          Address:
+          <textarea
+            name="address"
+            cols="30"
+            rows="10"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          Hobbies:
+          <input
+            type="checkbox"
+            name="isCricket"
+            onChange={this.handleChange}
+          />
+          Cricket
+          <input
+            type="checkbox"
+            name="isFootball"
+            onChange={this.handleChange}
+          />
+          Football
+        </div>
+        <div>
+          Countries:
+          <select name="country" onChange={this.handleChange}>
+            <option value="">Select</option>
+            <option value="india">India</option>
+            <option value="america">America</option>
+          </select>
+        </div>
+
+        <div>
+          File:
+          <input type="file" name="image" onChange={this.handleChange} />
+        </div>
+
+        <div>
+          <input type="button" value="send" onClick={this.fnClick} />
+        </div>
       </div>
     );
   }
